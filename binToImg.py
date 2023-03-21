751d1a4fb9e51b6ea7cb192e5a5151ace8af1be4
xinyew <wangxinye1996@outlook.com>
2023-03-21 11:27:13 -0400

    image converter done

--- binToImg.py
+++ binToImg.py
@@ -4,9 +4,11 @@ from keras.datasets import mnist
 import numpy as np
 from PIL import Image, ImageOps
 import os
+import sys
 
 import binToLabel
 
+# Helper function to save images
 def save_image(filename, data_array):
     im = Image.fromarray(data_array.astype('uint8'))
     im_invert = ImageOps.invert(im)
@@ -17,38 +19,36 @@ def loadMNIST():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     return x_train, y_train, x_test, y_test
 
-# DIR_NAME = "JPEGImages"
-# if os.path.exists(DIR_NAME) == False:
-#     os.mkdir(DIR_NAME)
 
-# # Save Images
-# i = 0
-# for li in [x_train, x_test]:
-#     print("[---------------------------------------------------------------]")
-#     for x in li:
-#         filename = "{0}/{1:05d}.jpg".format(DIR_NAME,i)
-#         print(filename)
-#         save_image(filename, x)
-#         i += 1
+# Convert MNIST bin files to images with labels
+def binToImg(target):
+    x_train, y_train, x_test, y_test = loadMNIST()
 
-def binToImg(target, x, y):
     if target == "test":
         DIR_NAME = "testDataset"
         SIZE = 10000
+        x = x_test
+        y = y_test
     elif target == "train":
         DIR_NAME = "trainDataset"
         SIZE = 60000
+        x = x_train
+        y = y_train
     else:
         raise Exception("Target has to be test or train")
     
+    if not os.path.exists(DIR_NAME):
+        os.mkdir(DIR_NAME)
+
     for i in range(SIZE):
-        pathName = "{0}/{1:01d}.jpg".format(DIR_NAME, y[i])
+        pathName = "{0}/{1:01d}".format(DIR_NAME, y[i])
         if not os.path.exists(pathName):
             os.mkdir(pathName)
         filename = "{0}/{1}.jpg".format(pathName, i)
         print(filename)
         save_image(filename, x[i])
 
+# Test function to verify the correctness of labels
 def _test():
     for i in range(20):
         filename = str(i)
@@ -57,5 +57,5 @@ def _test():
         print(binToLabel.getTestLabel(i, "test"))
         print()
 
-x_train, y_train, x_test, y_test = loadMNIST()
-binToImg("test", x_test, y_test)
+
+binToImg(sys.argv[1])
